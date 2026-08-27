b896d8aac6e3f72bd8e288f8e54455e1b749984d
Suneha Mariam <rehanagill260@gmail.com>
2026-08-27 14:28:10 +0500

    Implemented getBookByTitle and added output file

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -36,10 +36,17 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title.toLowerCase();
+  let matchingBooks = [];
+  
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].title.toLowerCase() === title) {
+      matchingBooks.push(books[isbn]);
+    }
+  });
+  
+  return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
 });
-
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
