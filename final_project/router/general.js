413abeac2a56a2b431c520ceab320a146bed8247
Suneha Mariam <rehanagill260@gmail.com>
2026-08-27 14:23:12 +0500

    Implemented getBookByAuthor and added output file

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,8 +22,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
 })
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let matchingBooks = [];
+  
+  Object.keys(books).forEach((isbn) => {
+    if (books[isbn].author === author) {
+      matchingBooks.push(books[isbn]);
+    }
+  });
+  
+  return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
 });
 
 // Get all books based on title
