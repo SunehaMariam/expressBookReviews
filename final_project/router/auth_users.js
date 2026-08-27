2cd349fe02e40fd0b078d7b39c940e10368eb4f9
Suneha Mariam <rehanagill260@gmail.com>
2026-08-27 15:08:42 +0500

    review added

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -61,6 +61,24 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     reviews: books[isbn].reviews
   });
 });
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+
+  if (!books[isbn]) {
+    return res.status(404).json({message: "Book not found"});
+  }
+
+  if (books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+    return res.status(200).json({
+      message: "Review deleted successfully",
+      reviews: books[isbn].reviews
+    });
+  } else {
+    return res.status(404).json({message: "No review found for this user"});
+  }
+});
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
